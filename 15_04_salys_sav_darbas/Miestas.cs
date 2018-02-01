790f5b8a464d292566e7d8d7a531fde2fab406ad
Vartotojas <Vartotojas@BERNARDAS>
2018-02-01 19:05:26 +0200

    uzduotys

---
+++ 15_04_salys_sav_darbas/Miestas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_04_salys_sav_darbas
+{
+    class Miestas
+    {
+        public string Pavadinimas { get; private set; }
+        public int GyventojuSkaicius { get; private set; }
+        public int PlotasKvKm { get; private set; }
+    }
+}
---
+++ 15_04_salys_sav_darbas/Pasaulis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_04_salys_sav_darbas
+{
+    class Pasaulis
+    {
+    }
+}
